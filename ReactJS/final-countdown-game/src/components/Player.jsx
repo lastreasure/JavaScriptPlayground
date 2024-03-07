1a49193cbf4dcf3c2f6c87730f051b237328ded5
lastreasure <43856727+lastreasure@users.noreply.github.com>
2024-03-07 11:26:31 +0000

    feat: 🎸 amending player with useRef and useState

--- ReactJS/final-countdown-game/src/components/Player.jsx
+++ ReactJS/final-countdown-game/src/components/Player.jsx
@@ -1,5 +1,14 @@
 import { useState, useRef } from 'react';
 
+    /* LEARNING NOTE - 2 - When to use useRef and useState
+    * When assigning a value using state we must evaluate whether we want 
+    * the code to rerender after being updated and be reflected in the UI
+    * (we would not use state for 'behind the scenes' values with no direct UI impact)
+    * e.g. here we wouldnt derive playername from a useRef
+    * as we need the component to be reevaluated and display playerName in the h2 once amended
+    * useRef would not reexecute the component function
+    * though we can useRef to gain direct DOM manipulation
+    */
 export default function Player() {
   const playerName = useRef();
 
@@ -7,6 +16,12 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    /* LEARNING NOTE - 1 - Caution around useRef manipulating DOM directly
+    * This assignment is more imperative than declarative code by changing the DOM directly 
+    * than letting React amend the DOM.
+    * We must be careful to not let useRef read and manipulate all values on the page
+    * as that deviates from the general idea of react
+    */
     playerName.current.value = '';
   }
 
