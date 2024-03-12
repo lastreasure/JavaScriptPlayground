edfe5893b209bc73aba2a50d1223faed304271b1
lastreasure <43856727+lastreasure@users.noreply.github.com>
2024-03-12 12:12:30 +0000

    feat: 🎸 extending use context and experiemetning with consumer

--- ReactJS/simple-shop/src/components/Cart.jsx
+++ ReactJS/simple-shop/src/components/Cart.jsx
@@ -12,8 +12,18 @@ export default function Cart({ onUpdateItemQuantity }) {
    *    const { items } = useContext(CartContext);
    *    {items.length === 0 && <p>No items in cart!</p>}
    */
-  const { items } = useContext(CartContext);
 
+
+  /**
+   * P4 - LEARNING NOTE - 2 - use Context Hook vs Consumer
+   * Instead of using the useContext hook
+   * you can wrap the component in Consumer, pass the jsx into a function 
+   * and access all the context state using this method
+   * Though this approach is a bit more cumbersome and more difficult to read
+   * so is often no the default approach
+   */
+
+  const { items } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -27,7 +37,6 @@ export default function Cart({ onUpdateItemQuantity }) {
         <ul id="cart-items">
           {items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
-
             return (
               <li key={item.id}>
                 <div>
@@ -52,5 +61,64 @@ export default function Cart({ onUpdateItemQuantity }) {
         Cart Total: <strong>{formattedTotalPrice}</strong>
       </p>
     </div>
-  );
+  )
 }
+
+
+
+   /**
+   * P4 - LEARNING NOTE - 2 - use Context Hook vs Consumer
+   * Instead of using the useContext hook
+   * you can wrap the component in Consumer, pass the jsx into a function 
+   * and access all the context state using this method
+   * Though this approach is a bit more cumbersome and more difficult to read
+   * so is often no the default approach
+   */
+  
+//   return (
+//     <CartContext.Consumer> 
+//       {(cartCtx) =>  {
+//           const totalPrice = cartCtx.items.reduce(
+//             (acc, item) => acc + item.price * item.quantity,
+//             0
+//           );
+//           const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+
+//           return (
+
+//         <div id="cart">
+//       {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+//       {cartCtx.items.length > 0 && (
+//         <ul id="cart-items">
+//           {cartCtx.items.map((item) => {
+//             const formattedPrice = `$${item.price.toFixed(2)}`;
+
+//             return (
+//               <li key={item.id}>
+//                 <div>
+//                   <span>{item.name}</span>
+//                   <span> ({formattedPrice})</span>
+//                 </div>
+//                 <div className="cart-item-actions">
+//                   <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+//                     -
+//                   </button>
+//                   <span>{item.quantity}</span>
+//                   <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+//                     +
+//                   </button>
+//                 </div>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       )}
+//       <p id="cart-total-price">
+//         Cart Total: <strong>{formattedTotalPrice}</strong>
+//       </p>
+//     </div>
+//           )
+//       }}
+//     </CartContext.Consumer>
+//   );
+// }
