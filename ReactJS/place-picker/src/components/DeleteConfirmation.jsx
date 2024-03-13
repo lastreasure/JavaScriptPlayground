cd590e257e8fa08cd091fcc100c1184e9ca9e3cf
lastreasure <43856727+lastreasure@users.noreply.github.com>
2024-03-13 17:35:39 +0000

    feat: 🎸 timer useeffect example for modal

--- ReactJS/place-picker/src/components/DeleteConfirmation.jsx
+++ ReactJS/place-picker/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,25 @@
+import {useEffect} from 'react'
+
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+  useEffect(() => {
+    console.log('TIMER SET');
+    // this function on it's own is not the prime issue, it's the clean up of this function
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+
+    /* P5 - LEARNING NOTE - 5 - useEffect clean up function
+     * Ensure that the timer is completed when the modal component is removed from the DOM
+     */
+    return () => {
+      console.log('Cleaning up timer');
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
