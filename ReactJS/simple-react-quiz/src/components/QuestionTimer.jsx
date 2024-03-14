25bf411ab6d220175791a4fe393d0997d615ee6f
lastreasure <43856727+lastreasure@users.noreply.github.com>
2024-03-14 18:25:29 +0000

    feat: 🎸 adding question timer

---
+++ ReactJS/simple-react-quiz/src/components/QuestionTimer.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+ const QuestionTimer = ({ timeout, onTimeout, mode }) => {
+  const [remainingTime, setRemainingTime] = useState(timeout);
+
+  useEffect(() => {
+    console.log('SETTING TIMEOUT');
+    const timer = setTimeout(onTimeout, timeout);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [timeout, onTimeout]);
+
+  useEffect(() => {
+    console.log('SETTING INTERVAL');
+    const interval = setInterval(() => {
+      setRemainingTime((prevRemainingTime) => prevRemainingTime - 100);
+    }, 100);
+
+    return () => {
+      // ensuring the inerval is cleanned up if the component is unmounted/runs more than once
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      // className={mode}
+    />
+  );
+}
+
+export default QuestionTimer;
--- ReactJS/simple-react-quiz/src/components/Quiz.jsx
+++ ReactJS/simple-react-quiz/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions'
 import quizCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from './QuestionTimer';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([])
@@ -8,31 +9,39 @@ const Quiz = () => {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
-  function handSelectAnswer(selectedAnswer) {
-    setUserAnswers((prevSelectedAnswers) => {
-      return [...prevSelectedAnswers, selectedAnswer]
-    })
-  }
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(selectedAnswer) {
+      setUserAnswers((prevSelectedAnswers) => {
+        return [...prevSelectedAnswers, selectedAnswer]
+      })
+  }, [])
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return <div id="summary"> 
     <img src={quizCompleteImg} alt='Trophy icon'/>
-    <h2>Quiz Compleed!</h2>
+    <h2>Quiz Completed!</h2>
     </div>
   }
 
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   // negative inputs will be swapped
   shuffledAnswers.sort(() => Math.random() - 0.5);
-  
+
   return (
     <div id="quiz">
       <div id="question">
+        {/* adding key here, so that the Questiontimer rerenders when key changes */}
+        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => 
           (<li key={answer} className='answer'>
-            <button onClick={() => handSelectAnswer(answer)}>
+            <button onClick={() => handleSelectAnswer(answer)}>
               {answer}
             </button> 
           </li>))}
